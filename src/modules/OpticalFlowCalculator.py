9bf3ff04870e9969906743bc859848b84b8ece42
Daniel Konecny <dkonecny97@gmail.com>
2021-10-12 21:51:02 +0200

    Refactoring of video synchronization and optical flow modules

---
+++ src/modules/OpticalFlowCalculator.py
@@ -0,0 +1,100 @@
+"""Self-Supervised Learning for Recognition of Sports Poses in Image - Master's Thesis Project
+Module for calculating optical flow in videos.
+Organisation: Brno University of Technology - Faculty of Information Technology
+Author: Daniel Konecny (xkonec75)
+Date: 12. 10. 2021
+"""
+
+import os
+from argparse import ArgumentParser
+from collections import deque
+
+import numpy as np
+import cv2
+
+
+def calc_optical_flow(old_frame, new_frame):
+    flow = cv2.optflow.calcOpticalFlowDenseRLOF(old_frame, new_frame, None)
+
+    horizontal_flow = flow[:, :, 0]
+    vertical_flow = flow[:, :, 1]
+
+    right_flow = horizontal_flow[horizontal_flow >= 0].sum()
+    left_flow = -horizontal_flow[horizontal_flow < 0].sum()
+    up_flow = vertical_flow[vertical_flow >= 0].sum()
+    down_flow = -vertical_flow[vertical_flow < 0].sum()
+
+    return [up_flow, down_flow, left_flow, right_flow]
+
+
+class OpticalFlowCalculator:
+    def __init__(self, directory, frame_skip=7):
+        self.directory = directory
+        self.files = []
+        self.frame_skip = frame_skip
+
+        for file in os.listdir(self.directory):
+            if file.endswith('.mp4'):
+                self.files.append(file)
+
+    def compute_and_save_flows(self):
+        print(f"Processing all files in {self.directory} directory.")
+        for file in self.files:
+            flow = self.process_video(file)
+            np.save(f'{self.directory}/{file.replace("mp4", "npy")}', flow)
+            print(f"Flow calculated and saved, number of frames: {len(flow)}.")
+
+    def process_video(self, file):
+        print(f"Calculating flow of {file} file.")
+
+        frame_queue = deque()
+
+        cap = cv2.VideoCapture(f'{self.directory}/{file}')
+        frame_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        # Read the first frame
+        for i in range(self.frame_skip):
+            ret, frame = cap.read()
+            if not ret:
+                print("Video shorter than frame skip.")
+                return np.array([])
+            frame_queue.append(frame)
+
+        video_flow = np.empty((frame_length - self.frame_skip, 4))
+
+        for frame_index in range(frame_length - self.frame_skip):
+            old_frame = frame_queue.popleft()
+
+            # Read the next frame
+            ret, new_frame = cap.read()
+            if not ret:
+                print("Frame length incorrectly computed.")
+                break
+            frame_queue.append(new_frame)
+
+            video_flow[frame_index] = calc_optical_flow(old_frame, new_frame)
+
+        return video_flow
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument(
+        'directory',
+        type=str,
+        help="Path to the directory with videos (without slash at the end).",
+    )
+    parser.add_argument(
+        '-f', '--frame_skip',
+        type=int,
+        default=7,
+        help="Number of frames in between optical flow is calculated."
+    )
+    args = parser.parse_args()
+
+    optical_flow_calc = OpticalFlowCalculator(args.directory, args.frame_skip)
+    optical_flow_calc.compute_and_save_flows()
+
+
+if __name__ == "__main__":
+    main()
--- src/modules/VideoSynchronizer.py
+++ src/modules/VideoSynchronizer.py
@@ -1,105 +1,21 @@
+"""Self-Supervised Learning for Recognition of Sports Poses in Image - Master's Thesis Project
+Module for synchronization of multiple videos of the same scene.
+Organisation: Brno University of Technology - Faculty of Information Technology
+Author: Daniel Konecny (xkonec75)
+Date: 12. 10. 2021
+"""
+
+import os
+from argparse import ArgumentParser
+
 import numpy as np
 from scipy.stats import pearsonr
+import cv2
+
+import OpticalFlowCalculator
 
 
-def main():
-    fps = 29.97
-    video1 = 'out/flow_20211009_0.npy'
-    video2 = 'out/flow_20211009_2.npy'
-
-    flow1 = np.load(video1)
-    flow2 = np.load(video2)
-
-    # flow1 = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-    # flow2 = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-
-    if len(flow1) > len(flow2):
-        print("Videos switched due to length (first has to be shorter than second).")
-        temp = flow1
-        flow1 = flow2
-        flow2 = temp
-
-        temp = video1
-        video1 = video2
-        video2 = temp
-
-    len1 = len(flow1)
-    len2 = len(flow2)
-
-    # Number of frames that surely overlay.
-    overlay = 5000
-
-    correlation = np.empty((len1 + len2 - 2*overlay + 1, 4))
-
-    for i in range(overlay, len1 + len2 - overlay + 1):
-        if i <= len1:
-            correlation[i - overlay][0], _ = pearsonr(flow1[len1 - i:, 0], flow2[:i, 0])
-            correlation[i - overlay][1], _ = pearsonr(flow1[len1 - i:, 1], flow2[:i, 1])
-            correlation[i - overlay][2], _ = pearsonr(flow1[len1 - i:, 2], flow2[:i, 2])
-            correlation[i - overlay][3], _ = pearsonr(flow1[len1 - i:, 3], flow2[:i, 3])
-            # print(f"1 size: {flow1[len1 - i:].shape} - {flow2[:i].shape}")
-            # print(f"1 elements: {flow1[len1 - i:]} - {flow2[:i]}")
-        elif i <= len2:
-            correlation[i - overlay][0], _ = pearsonr(flow1[:, 0], flow2[i - len1:i, 0])
-            correlation[i - overlay][1], _ = pearsonr(flow1[:, 1], flow2[i - len1:i, 1])
-            correlation[i - overlay][2], _ = pearsonr(flow1[:, 2], flow2[i - len1:i, 2])
-            correlation[i - overlay][3], _ = pearsonr(flow1[:, 3], flow2[i - len1:i, 3])
-            # print(f"2 size: {flow1[:].shape} - {flow2[i - len1:i].shape}")
-            # print(f"2 elements: {flow1[:]} - {flow2[i - len1:i]}")
-        else:
-            correlation[i - overlay][0], _ = pearsonr(flow1[:len1 - (i - len2), 0], flow2[i - len1:, 0])
-            correlation[i - overlay][1], _ = pearsonr(flow1[:len1 - (i - len2), 1], flow2[i - len1:, 1])
-            correlation[i - overlay][2], _ = pearsonr(flow1[:len1 - (i - len2), 2], flow2[i - len1:, 2])
-            correlation[i - overlay][3], _ = pearsonr(flow1[:len1 - (i - len2), 3], flow2[i - len1:, 3])
-            # print(f"3 size: {flow1[:len1 - (i - len2)].shape} - {flow2[i - len1:].shape}")
-            # print(f"3 elements: {flow1[:len1 - (i - len2)]} - {flow2[i - len1:]}")
-
-    sum_correlation = np.abs(correlation).sum(axis=1)
-    best_match = np.argpartition(sum_correlation, -1)[-1:][0]
-
-    print(f"Length of first video: {len1}.")
-    print(f"Length of second video: {len2}.")
-    print(f"Overlay of videos taken into account: {overlay}.")
-    print(f"Number of possible video combinations: {len(correlation)}.")
-    print(f"Index of best match: {best_match}.")
-
-    new_len1 = len1
-    new_len2 = len2
-
-    difference = overlay + best_match - len1
-    if difference < 0:
-        difference = -difference
-        new_len1 -= difference
-        print(f"Cut video {video1} from ", end="")
-    elif difference > 0:
-        new_len2 -= difference
-        print(f"Cut video {video2} from ", end="")
-    else:
-        print("Videos start at the same time.")
-
-    seconds = difference / fps
-    milliseconds = int((seconds % 1)*1000)
-    hours = 0
-    minutes = 0
-    if seconds > 3600:
-        hours = int(seconds // 3600)
-        seconds %= 3600
-    if seconds > 60:
-        minutes = int(seconds // 60)
-        seconds %= 60
-    print(f"{hours:02d}:{minutes:02d}:{int(seconds):02d}.{milliseconds:03d}.")
-
-    cut_after = 0
-    if new_len1 > new_len2:
-        cut_after = new_len2
-        print(f"Cut video {video1} after (duration) ", end="")
-    elif new_len1 < new_len2:
-        cut_after = new_len1
-        print(f"Cut video {video2} after (duration) ", end="")
-    else:
-        print("Videos end at the same time.")
-
-    seconds = cut_after / fps
+def get_timestamp_from_seconds(seconds):
     milliseconds = int((seconds % 1) * 1000)
     hours = 0
     minutes = 0
@@ -109,10 +25,171 @@ def main():
     if seconds > 60:
         minutes = int(seconds // 60)
         seconds %= 60
-    print(f"{hours:02d}:"
-          f"{minutes:02d}:"
-          f"{int(seconds):02d}."
-          f"{milliseconds:03d}.")
+    return f"{hours:02d}:{minutes:02d}:{int(seconds):02d}.{milliseconds:03d}."
+
+
+class VideoSynchronizer:
+    def __init__(self, directory, overlay=1000):
+        self.directory = directory
+        self.overlay = overlay
+        self.videos = []
+        self.flows = []
+        self.differences = [0]
+        self.fps = []
+
+    def load_flows(self):
+        print("Loading flows.")
+
+        for file in os.listdir(self.directory):
+            if file.endswith('.npy'):
+                self.flows.append(np.load(f'{self.directory}/{file}'))
+                self.videos.append(file.replace("npy", "mp4"))
+
+        print(f"Flows loaded: {len(self.flows)}.")
+
+    def calculate_flows(self):
+        print("Calculating flows.")
+
+        optical_flow_calc = OpticalFlowCalculator.OpticalFlowCalculator(self.directory)
+
+        for file in os.listdir(self.directory):
+            if file.endswith('.mp4'):
+                flow = optical_flow_calc.process_video(file)
+                self.flows.append(flow)
+                self.videos.append(file)
+
+        print(f"Flows calculated: {len(self.flows)}.")
+
+    def check_length(self):
+        print("Checking length of flows.")
+
+        flow1 = self.flows[0]
+        flow2 = self.flows[1]
+
+        video1 = self.videos[0]
+        video2 = self.videos[1]
+
+        if len(flow1) > len(flow2):
+            print("Videos switched due to length (first has to be shorter than second).")
+            self.flows[0] = flow2
+            self.flows[1] = flow1
+
+            self.videos[0] = video2
+            self.videos[1] = video1
+
+    def calc_correlation(self):
+        print("Calculating correlation of flows.")
+
+        flow1 = self.flows[0]
+        flow2 = self.flows[1]
+
+        len1 = len(flow1)
+        len2 = len(flow2)
+
+        if self.overlay > len1 or self.overlay > len2:
+            print("Overlay too big, automatically decreased to default value 1000.")
+            self.overlay = 1000
+
+        correlation = np.empty((len1 + len2 - 2 * self.overlay + 1, 4))
+
+        for i in range(self.overlay, len1 + len2 - self.overlay + 1):
+            if i <= len1:
+                correlation[i - self.overlay][0], _ = pearsonr(flow1[len1 - i:, 0], flow2[:i, 0])
+                correlation[i - self.overlay][1], _ = pearsonr(flow1[len1 - i:, 1], flow2[:i, 1])
+                correlation[i - self.overlay][2], _ = pearsonr(flow1[len1 - i:, 2], flow2[:i, 2])
+                correlation[i - self.overlay][3], _ = pearsonr(flow1[len1 - i:, 3], flow2[:i, 3])
+            elif i <= len2:
+                correlation[i - self.overlay][0], _ = pearsonr(flow1[:, 0], flow2[i - len1:i, 0])
+                correlation[i - self.overlay][1], _ = pearsonr(flow1[:, 1], flow2[i - len1:i, 1])
+                correlation[i - self.overlay][2], _ = pearsonr(flow1[:, 2], flow2[i - len1:i, 2])
+                correlation[i - self.overlay][3], _ = pearsonr(flow1[:, 3], flow2[i - len1:i, 3])
+            else:
+                correlation[i - self.overlay][0], _ = pearsonr(flow1[:len1 - (i - len2), 0], flow2[i - len1:, 0])
+                correlation[i - self.overlay][1], _ = pearsonr(flow1[:len1 - (i - len2), 1], flow2[i - len1:, 1])
+                correlation[i - self.overlay][2], _ = pearsonr(flow1[:len1 - (i - len2), 2], flow2[i - len1:, 2])
+                correlation[i - self.overlay][3], _ = pearsonr(flow1[:len1 - (i - len2), 3], flow2[i - len1:, 3])
+
+        return correlation
+
+    def calc_difference(self, correlation):
+        flow1 = self.flows[0]
+        flow2 = self.flows[1]
+
+        sum_correlation = np.abs(correlation).sum(axis=1)
+        best_match = np.argpartition(sum_correlation, -1)[-1:][0]
+
+        print(f"Length of first video: {len(flow1)}.")
+        print(f"Length of second video: {len(flow2)}.")
+        print(f"Overlay of videos taken into account: {self.overlay}.")
+        print(f"Number of possible video combinations: {len(correlation)}.")
+        print(f"Index of best match: {best_match}.")
+
+        self.differences.append(self.overlay + best_match - len(flow1))
+
+    def get_fps(self):
+        print("Getting fps.")
+        for video in self.videos:
+            cap = cv2.VideoCapture(f'{self.directory}/{video}')
+            self.fps.append(cap.get(cv2.CAP_PROP_FPS))
+            print(f"Video {video} has {self.fps[-1]} fps.")
+
+    def calc_cuts(self):
+        new_len1 = len(self.flows[0])
+        new_len2 = len(self.flows[1])
+
+        self.get_fps()
+
+        difference = self.differences[1]
+
+        if difference < 0:
+            new_len1 -= -difference
+            print(f"Cut video {self.videos[0]} from {get_timestamp_from_seconds(-difference / self.fps[0])}")
+        elif difference > 0:
+            new_len2 -= difference
+            print(f"Cut video {self.videos[1]} from {get_timestamp_from_seconds(difference / self.fps[1])}")
+        else:
+            print("Videos start at the same time.")
+
+        if new_len1 > new_len2:
+            print(f"Cut video {self.videos[0]} after (duration) {get_timestamp_from_seconds(new_len2 / self.fps[0])}")
+        elif new_len1 < new_len2:
+            print(f"Cut video {self.videos[1]} after (duration) {get_timestamp_from_seconds(new_len1 / self.fps[1])}")
+        else:
+            print("Videos end at the same time.")
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument(
+        'directory',
+        type=str,
+        help="Path to the directory with videos (without slash at the end).",
+    )
+    parser.add_argument(
+        '-o', '--overlay',
+        type=int,
+        default=1000,
+        help="Number of frames that have to overlay."
+    )
+    parser.add_argument(
+        '-l', '--load',
+        action='store_true',
+        help="Use when optical flow has already been calculated and can only be loaded."
+    )
+    args = parser.parse_args()
+
+    video_synchronizer = VideoSynchronizer(args.directory, args.overlay)
+
+    if args.load:
+        video_synchronizer.load_flows()
+    else:
+        video_synchronizer.calculate_flows()
+
+    video_synchronizer.check_length()
+
+    correlation = video_synchronizer.calc_correlation()
+    video_synchronizer.calc_difference(correlation)
+    video_synchronizer.calc_cuts()
 
 
 if __name__ == "__main__":
