e5fe9d60270458764275dc70c097d5b9aa95e9df
Daniel Konecny <dkonecny97@gmail.com>
2021-11-01 12:50:35 +0100

    Any rectangular crop supported by VideoCropper

--- src/modules/VideoCropper.py
+++ src/modules/VideoCropper.py
@@ -2,10 +2,12 @@
 Module for cropping of video to a selected area.
 Organisation: Brno University of Technology - Faculty of Information Technology
 Author: Daniel Konecny (xkonec75)
-Date: 31. 10. 2021
-Source: https://stackoverflow.com/questions/6136588/image-cropping-using-python/8696558
+Date: 01. 11. 2021
+Source: select crop area using pygame library
+    (https://stackoverflow.com/questions/6136588/image-cropping-using-python/8696558)
 """
 
+import sys
 import os
 from argparse import ArgumentParser
 import numpy as np
@@ -16,9 +18,7 @@ import contextlib
 with contextlib.redirect_stdout(None):
     import pygame
 
-
-# TODO - Adjust to be able to have any result ratio, not just square.
-# TODO - Form as an interface to get the right dimensions
+COMPUTATIONAL_PRECISION = 1e6
 
 
 def crop_video(crop_command):
@@ -36,12 +36,27 @@ def create_script(crop_command):
 
 
 class VideoCropper:
-    def __init__(self, video, result_w, result_h):
-        self.video = video
+    def __init__(self, video_path, fps, result_w, result_h, scene_num, video_num):
+        self.video_path = video_path
+        self.directory = os.path.dirname(video_path)
+
+        self.result_fps = fps
         self.result_w = result_w
         self.result_h = result_h
+        if result_w % 2 != 0:
+            self.result_w += 1
+            print(f"Resulting width has to be even, changed from {result_w} to {self.result_w}.", file=sys.stderr)
+        if result_h % 2 != 0:
+            self.result_h += 1
+            print(f"Resulting height has to be even, changed from {result_h} to {self.result_h}.", file=sys.stderr)
+        self.ratio = int(self.result_w / self.result_h * COMPUTATIONAL_PRECISION)
+
+        self.scene_num = scene_num
+        self.video_num = video_num
+
         self.px = None
         self.screen = None
+
         self.video_w = self.video_h = 0
         self.left = self.right = self.upper = self.lower = 0
 
@@ -106,77 +121,70 @@ class VideoCropper:
             print("-- Upper and lower coordinates swapped.")
             self.lower, self.upper = self.upper, self.lower
 
-    def check_crop_proportions(self):
-        print("- Checking if crop selection is a square.")
+    def check_crop_ratio(self):
+        print("- Checking if crop selection has a correct ratio.")
+
         crop_width = self.right - self.left
         crop_height = self.lower - self.upper
-        if crop_width > crop_height:
-            print("-- Increasing height to form a square.")
-            difference_cw_ch = crop_width - crop_height
-            half_difference_cw_ch = difference_cw_ch // 2
-            if difference_cw_ch % 2 == 0:
-                self.upper -= half_difference_cw_ch
-                self.lower += half_difference_cw_ch
-            else:
-                self.upper -= half_difference_cw_ch
-                self.lower += half_difference_cw_ch + 1
-        elif crop_width < crop_height:
-            print("-- Increasing width to form a square.")
-            difference_ch_cw = crop_height - crop_width
-            half_difference_ch_cw = difference_ch_cw // 2
-            if difference_ch_cw % 2 == 0:
-                self.left -= half_difference_ch_cw
-                self.right += half_difference_ch_cw
-            else:
-                self.left -= half_difference_ch_cw
-                self.right += half_difference_ch_cw + 1
+        crop_ratio = int((crop_width / crop_height) * COMPUTATIONAL_PRECISION)
+
+        if crop_ratio > self.ratio:
+            new_height = int((crop_width / self.ratio) * COMPUTATIONAL_PRECISION)
+            print(f"-- Increasing height to {new_height} to fit the ratio.")
+            half_difference = (new_height - crop_height) // 2
+            self.upper -= half_difference
+            self.lower += half_difference
+            if (new_height - crop_height) % 2 != 0:
+                self.lower += 1
+
+        elif crop_ratio < self.ratio:
+            new_width = int(crop_height * self.ratio / COMPUTATIONAL_PRECISION)
+            print(f"-- Increasing width to {new_width} to fit the ratio.")
+            half_difference = (new_width - crop_width) // 2
+            self.left -= half_difference
+            self.right += half_difference
+            if (new_width - crop_width) % 2 != 0:
+                self.right += 1
 
     def check_crop_size_to_result(self):
         print("- Checking if crop selection is large enough.")
         crop_size = self.right - self.left  # Width and height are now equal.
         if crop_size < self.result_w:  # Goal width and height are equal.
             print("-- Increasing crop selection size.")
-            difference = self.result_w - crop_size
-            half_difference = difference // 2
-            if difference % 2 == 0:
-                self.left -= half_difference
-                self.right += half_difference
-                self.upper -= half_difference
-                self.lower += half_difference
-            else:
-                self.left -= half_difference
-                self.right += half_difference + 1
-                self.upper -= half_difference
-                self.lower += half_difference + 1
+            half_difference = (self.result_w - crop_size) // 2
+            self.left -= half_difference
+            self.right += half_difference
+            self.upper -= half_difference
+            self.lower += half_difference
+            if (self.result_w - crop_size) % 2 != 0:
+                self.right += 1
+                self.lower += 1
 
     def check_crop_size_to_frame(self):
         print("- Checking if crop selection did not exceed the frame size.")
         crop_size = self.right - self.left  # Width and height are now equal.
         if crop_size > self.video_w or crop_size > self.video_h:
-            difference = 0
+            difference = crop_size
             if crop_size > self.video_w:
                 print("-- Width too high, decreasing both dimensions.")
-                difference = crop_size - self.video_w
+                difference -= self.video_w
             if crop_size > self.video_h:
                 print("-- Height too high, decreasing both dimensions.")
-                difference = crop_size - self.video_h
+                difference -= self.video_h
             half_difference = difference // 2
-            if difference % 2 == 0:
-                self.left += half_difference
-                self.right -= half_difference
-                self.upper += half_difference
-                self.lower -= half_difference
-            else:
-                self.left += half_difference
-                self.right -= half_difference + 1
-                self.upper += half_difference
-                self.lower -= half_difference + 1
+            self.left += half_difference
+            self.right -= half_difference
+            self.upper += half_difference
+            self.lower -= half_difference
+            if difference % 2 != 0:
+                self.right -= 1
+                self.lower -= 1
 
     def check_crop_position(self):
         print("- Checking if crop selection is not out of the frame.")
         if self.left < 0:
             print("-- Moving crop to the right.")
-            self.right += -self.left
+            self.right -= self.left
             self.left = 0
         elif self.right > self.video_w:
             print("-- Moving crop to the left.")
@@ -184,7 +192,7 @@ class VideoCropper:
             self.right = self.video_w
         if self.upper < 0:
             print("-- Moving crop down.")
-            self.lower += -self.upper
+            self.lower -= self.upper
             self.upper = 0
         elif self.lower > self.video_h:
             print("-- Moving crop up.")
@@ -193,32 +201,33 @@ class VideoCropper:
 
     def recalculate_crops(self):
         print("\nRecalculating crop area...")
-        print("- Original coordinates and information")
+        print("- Video information")
         print(f"-- Frame width - {self.video_w}")
         print(f"-- Frame height - {self.video_h}")
+        print(f"-- Ratio * {COMPUTATIONAL_PRECISION}- {self.ratio}")
+        print("- Original crop coordinates")
         print(f"-- Left - {self.left}")
         print(f"-- Right - {self.right}")
         print(f"-- Upper - {self.upper}")
         print(f"-- Lower - {self.lower}")
 
         self.check_crop_direction()
-        self.check_crop_proportions()
+        self.check_crop_ratio()
         self.check_crop_size_to_result()
         self.check_crop_size_to_frame()
         self.check_crop_position()
 
-        print("- Recalculated coordinates")
+        print("- Recalculated crop coordinates")
         print(f"-- Left - {self.left}")
         print(f"-- Right - {self.right}")
         print(f"-- Upper - {self.upper}")
         print(f"-- Lower - {self.lower}")
 
-    def get_image(self):
+    def get_image(self, image_count=10):
         print("Merging video to single image...")
-        image_count = 5
         image_channels = 3
 
-        video = cv2.VideoCapture(f'{self.video}')
+        video = cv2.VideoCapture(f'{self.video_path}')
         self.video_w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
         self.video_h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
         frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -230,7 +239,7 @@ class VideoCropper:
             video.set(cv2.CAP_PROP_POS_FRAMES, frame_to_use)
             ret, frame = video.read()
             if not ret:
-                print(f"-- Frame from video could not be obtained.")
+                print(f"-- Frame from video could not be obtained.", file=sys.stderr)
             frame = frame.astype(np.float32) / 255.
             merged_image = np.add(merged_image, frame / image_count)
 
@@ -248,18 +257,28 @@ class VideoCropper:
         cropped_image = image_to_crop[self.upper:self.lower, self.left:self.right]
         cv2.imwrite(output, cropped_image)
 
-    def get_crop_commands(self, framerate):
-        print("Preparing command to crop the video...")
+    def get_crop_commands(self):
+        """
+        ffmpeg command to crop a video:
+            ffmpeg
+            -i input_file
+            -filter:v "crop=width:height:start_width:start_height, scale=width:height, fps=fps"
+            -an
+            output_file
+
+        :return: String with constructed ffmpeg command to crop and scale the video.
+        """
+        print("\nPreparing command to crop the video...")
         crop_command = ""
 
         crop_command += f'\nffmpeg \\\n'
-        crop_command += f'-i {self.video} \\\n'
+        crop_command += f'-i {self.video_path} \\\n'
         crop_command += f'-filter:v "\\\n'
         crop_command += f'crop={self.right - self.left}:{self.lower - self.upper}:{self.left}:{self.upper}, \\\n'
         crop_command += f'scale={self.result_w}:{self.result_h}, \\\n'
-        crop_command += f'fps={framerate}" \\\n'
+        crop_command += f'fps={self.result_fps}" \\\n'
         crop_command += f'-an \\\n'
-        crop_command += f'{self.video.replace(".mp4", "_normalized.mp4")}\n'
+        crop_command += f'{self.directory}/scene{self.scene_num:03d}_video{self.video_num}_normalized.mp4\n'
 
         return crop_command
 
@@ -270,6 +289,18 @@ if __name__ == "__main__":
         'video',
         type=str,
         help="Path to the video to be cropped.",
+    )
+    parser.add_argument(
+        '-S', '--scene_num',
+        type=int,
+        default=0,
+        help="Number of this dataset scene.",
+    )
+    parser.add_argument(
+        '-V', '--video_num',
+        type=int,
+        default=0,
+        help="Number of this video in a scene.",
     )
     parser.add_argument(
         '-W', '--width',
@@ -284,7 +315,7 @@ if __name__ == "__main__":
         help="height of the resulting video.",
     )
     parser.add_argument(
-        '-f', '--framerate',
+        '-F', '--framerate',
         type=int,
         default=20,
         help="height of the resulting video.",
@@ -298,14 +329,14 @@ if __name__ == "__main__":
 
     pygame.init()
 
-    video_cropper = VideoCropper(args.video, args.width, args.height)
+    video_cropper = VideoCropper(args.video, args.framerate, args.width, args.height, args.scene_num, args.video_num)
     image = video_cropper.get_image()
     video_cropper.set_crops()
 
     pygame.display.quit()
 
     video_cropper.recalculate_crops()
-    command = video_cropper.get_crop_commands(args.framerate)
+    command = video_cropper.get_crop_commands()
     if args.script:
         create_script(command)
     else:
