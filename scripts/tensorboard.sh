8a25a57cba8f3c535d5128e338f1cc07add8280a
Daniel Konecny <dkonecny97@gmail.com>
2021-11-24 21:50:38 +0100

    Add script as a hint for TensorBoard

---
+++ scripts/tensorboard.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Self-Supervised Learning for Recognition of Sports Poses in Image - Master's Thesis Project
+# Script for operating with TensorBoard.
+# Organisation: Brno University of Technology - Faculty of Information Technology
+# Author: Daniel Konecny (xkonec75)
+# Date: 24. 11. 2021
+
+tensorboard --logdir "directory"
+
+# UPLOAD TO tensorboard.dev
+#tensorboard dev upload --logdir "directory" --name "board_name"
