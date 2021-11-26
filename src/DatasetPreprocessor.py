72f1350d809bef1c3678bbcd20066e5275fb180d
Daniel Konecny <dkonecny97@gmail.com>
2021-11-26 15:33:54 +0100

    Add new modules for future use

---
+++ src/DatasetPreprocessor.py
@@ -0,0 +1,7 @@
+"""
+Self-Supervised Learning for Recognition of Sports Poses in Image - Master's Thesis Project
+Preprocess the whole dataset from raw video data using the available modules.
+Organisation: Brno University of Technology - Faculty of Information Technology
+Author: Daniel Konecny (xkonec75)
+Date: 26. 11. 2021
+"""
---
+++ src/model/Recognizer.py
@@ -0,0 +1,7 @@
+"""
+Self-Supervised Learning for Recognition of Sports Poses in Image - Master's Thesis Project
+Recognize image from a latent vector.
+Organisation: Brno University of Technology - Faculty of Information Technology
+Author: Daniel Konecny (xkonec75)
+Date: 26. 11. 2021
+"""
