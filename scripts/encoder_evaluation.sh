dac87f82cafc4179550c738095ed6087a41418bc
Daniel Konecny <dkonecny97@gmail.com>
2022-06-26 13:57:17 +0200

    Fix encoder eval script

--- scripts/encoder_evaluation.sh
+++ scripts/encoder_evaluation.sh
@@ -10,6 +10,8 @@ for dimension in 64 128 256 512
 do
     for seed in {0..4}
     do
+        mkdir logs/latent-$dimension-"$seed"
+
         python3 src/model/Encoder.py "data/upper_body" -e 50 -f 0 -m 0.1 -b 64 -s 0.1 -d "$dimension" -S "$seed" -E \
         -ed ckpts/encoder/latent-$dimension-"$seed" -ld logs/latent-$dimension-"$seed" -v -g \
         2> logs/latent-$dimension-"$seed"/err.log | tee logs/latent-$dimension-"$seed"/out.log
