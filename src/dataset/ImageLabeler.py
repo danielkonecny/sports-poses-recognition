2c2c94934d313acb284bc6fdabc880e02d2a64d5
Daniel Konecny <dkonecny97@gmail.com>
2022-06-29 18:20:41 +0200

    Extend and fix some things

--- src/dataset/ImageLabeler.py
+++ src/dataset/ImageLabeler.py
@@ -3,7 +3,7 @@ Self-Supervised Learning for Recognition of Sports Poses in Image - Master's The
 Module for labeling images.
 Organisation: Brno University of Technology - Faculty of Information Technology
 Author: Daniel Konecny (xkonec75)
-Date: 15. 06. 2022
+Date: 28. 06. 2022
 """
 
 from argparse import ArgumentParser
@@ -11,6 +11,9 @@ from pathlib import Path
 import pickle
 
 import cv2
+
+CAM_COUNT = 3
+CAM_SHOWN = 1
 
 
 def parse_arguments():
@@ -22,58 +25,67 @@ def parse_arguments():
              "Unsorted in a directory named \"sort\"."
     )
     parser.add_argument(
-        'classes',
+        '-c', '--classes',
         type=str,
+        default=None,
         help="Path to the dictionary with classes saved with pickle."
     )
     return parser.parse_args()
 
 
-def import_classes(classes_location):
-    if classes_location is not None:
-        with open(classes_location, 'rb') as f:
-            classes = pickle.load(f)
-    else:
-        classes = {32: "unsorted"}
-
-    return classes
-
-
 class ImageLabeler:
     def __init__(self, directory, classes):
         self.directory = Path(directory)
-        self.image_paths = (self.directory / "sort").rglob('*.png')
-        self.classes = import_classes(classes)
+        self.image_paths = (self.directory / "sort").rglob(f'*/cam{CAM_SHOWN}/*.png')
+        self.classes = self.import_classes(classes)
 
-        print("Image Labeler (IM) initialized.")
+        print("Image Labeler (IL) initialized.")
 
     def label(self):
-        print("IM - Labeling images...")
-        print("IM -- \"Esc\" - quit labeling.")
+        print("IL - Labeling images...")
+        print("IL -- \"Esc\" - quit labeling.")
         self.print_classes()
+
+        cv2.namedWindow("Image")
 
         for image_path in self.image_paths:
             image = cv2.imread(str(image_path))
-            cv2.namedWindow("Image")
             cv2.imshow("Image", image)
-            k = cv2.waitKey(0) & 0xff
-            if k == 27:
-                break
-            elif k not in self.classes:
-                class_name = input(f"IM --- Key: \"{chr(k)}\" = Class: ")
-                self.classes[k] = class_name
+            k1 = cv2.waitKey(0) & 0xff
+            cv2.imshow("Image", image)
+            k2 = cv2.waitKey(0) & 0xff
 
-            dst_dir = self.directory / self.classes[k]
-            dst = dst_dir / image_path.name
-            Path(dst_dir).mkdir(parents=True, exist_ok=True)
-            image_path.replace(dst)
+            if k1 == 27 or k2 == 27:
+                break
+
+            k = chr(k1) + chr(k2)
+            if k not in self.classes:
+                while True:
+                    class_name = input(f"IL --- Key: \"{k}\" = Class: ")
+                    if class_name == "sort":
+                        print(f"IL --- Cannot name a class \"sort\", that is a default name for directory "
+                              f"of unsorted images.")
+                    else:
+                        self.classes[k] = class_name
+                        break
+
+            # TODO - Make more general for other directory structures than scene/cam/img.png.
+            for cam_index in range(0, CAM_COUNT):
+                new_image_name = image_path.name.replace(f"cam{CAM_SHOWN}", f"cam{cam_index}")
+
+                dst_dir = self.directory / self.classes[k]
+                dst = dst_dir / new_image_name
+                Path(dst_dir).mkdir(parents=True, exist_ok=True)
+
+                general_image_path = image_path.parents[1] / f"cam{cam_index}" / new_image_name
+                general_image_path.replace(dst)
 
         cv2.destroyWindow("Image")
 
     def edit(self):
         self.print_classes()
 
-        k = ord(input(f"Enter the key of class name you want to change (or add): "))
+        k = input(f"Enter the key of class name you want to change (or add): ")
         c = input(f"Enter the new class name (or nothing to delete): ")
 
         if c == "":
@@ -93,15 +105,25 @@ class ImageLabeler:
                 dst_dir = self.directory / self.classes[k]
                 dst_dir.mkdir()
 
+    @staticmethod
+    def import_classes(classes_location):
+        if classes_location is not None:
+            with open(classes_location, 'rb') as f:
+                classes = pickle.load(f)
+        else:
+            classes = {"  ": "unsorted"}
+
+        return classes
+
     def export_classes(self):
         file_name = self.directory / 'classes.pkl'
         with open(file_name, 'wb') as file:
             pickle.dump(self.classes, file)
 
     def print_classes(self):
-        print("IM -- Available classes:")
+        print("IL -- Available classes:")
         for k, c in self.classes.items():
-            print(f"IM --- Key: \"{chr(k)}\" = Class: {c}")
+            print(f"IL --- Key: \"{k}\" = Class: {c}")
 
 
 def main():
@@ -110,10 +132,10 @@ def main():
     image_labeler = ImageLabeler(args.dir, args.classes)
 
     while True:
-        print("IM - Choose your task.")
-        print("IM -- Enter \"1\" to label images.")
-        print("IM -- Enter \"2\" to edit labels.")
-        print("IM -- Enter \"0\" to exit.")
+        print("IL - Choose your task.")
+        print("IL -- Enter \"1\" to label images.")
+        print("IL -- Enter \"2\" to edit labels.")
+        print("IL -- Enter \"0\" to exit.")
 
         k = input()
 
@@ -128,7 +150,7 @@ def main():
 
         image_labeler.export_classes()
 
-        print("IM - Task finished.")
+        print("IL - Task finished.")
 
 
 if __name__ == "__main__":
