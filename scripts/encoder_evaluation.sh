e35431d3e5eb54633a7f3600383658f12450e35b
Daniel Konecny <dkonecny97@gmail.com>
2022-07-05 22:33:23 +0200

    Update scripts for automatic evaluation

--- scripts/encoder_evaluation.sh
+++ scripts/encoder_evaluation.sh
@@ -4,15 +4,15 @@
 # Script for launching a model training with different parameters to evaluate it.
 # Organisation: Brno University of Technology - Faculty of Information Technology
 # Author: Daniel Konecny (xkonec75)
-# Date: 26. 06. 2022
+# Date: 05. 07. 2022
 
-for dimension in 64 128 256 512
+for dimension in 16 32 64 128 256 512
 do
     for seed in {0..4}
     do
         mkdir logs/latent-$dimension-"$seed"
 
-        python3 src/model/Encoder.py "data/upper_body" -e 50 -f 0 -m 0.1 -b 64 -s 0.1 -d "$dimension" -S "$seed" -E \
+        python3 src/model/Encoder.py "data/upper_body/self_supervised" -e 50 -f 0 -m 0.1 -b 64 -s 0.1 -d "$dimension" -S "$seed" -E \
         -ed ckpts/encoder/latent-$dimension-"$seed" -ld logs/latent-$dimension-"$seed" -v -g \
         2> logs/latent-$dimension-"$seed"/err.log | tee logs/latent-$dimension-"$seed"/out.log
     done
--- scripts/supervised_evaluation.sh
+++ scripts/supervised_evaluation.sh
@@ -4,12 +4,12 @@
 # Script for launching a model training with different parameters to evaluate it.
 # Organisation: Brno University of Technology - Faculty of Information Technology
 # Author: Daniel Konecny (xkonec75)
-# Date: 22. 06. 2022
+# Date: 05. 07. 2022
 
-for split in 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+for split in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.825 0.85 0.875 0.9 0.925 0.95 0.975
 do
     for seed in {0..9}
     do
-        python3 src/model/Supervised.py "data/simple" -e 50 -b 128 -s "$split" -S "$seed" -E -v -g
+        python3 src/model/Supervised.py "data/upper_body/labeled_directions" -e 50 -b 128 -s "$split" -S "$seed" -E -v -g
     done
 done
